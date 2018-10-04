ef8339cb59074922a788b4ebbc85e945151faf41
Jacob Brunson <jacob@brunson.me>
2018-10-04 13:29:58 -0500

    added button event handlers and created method for changing connection status

--- RePlay/NavigationFragment.cs
+++ RePlay/NavigationFragment.cs
@@ -16,18 +16,63 @@ namespace RePlay
 {
     public class NavigationFragment : Fragment
     {
+        private void homeClicked(object sender, EventArgs e)
+        {
+            ImageButton button = (ImageButton)sender;
+            Intent intent = new Intent(button.Context, typeof(MainActivity));
+            button.Context.StartActivity(intent);
+        }
+
+        private void gamesClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("navigate to games launcher");
+        }
+
+        private void connectionClicked(object sender, EventArgs e)
+        {
+            IsConnected = !IsConnected;
+        }
+
+        private void settingsClicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("navigate to settings screen");
+        }
+
+        private bool isConnected = true;
+        public bool IsConnected {
+            get { return isConnected; }
+            set
+            {
+                isConnected = value;
+                if (isConnected) {
+                    connectionButton.SetImageResource(Resource.Drawable.pcmConnected);
+                } else {
+                    connectionButton.SetImageResource(Resource.Drawable.pcmDisconnected);
+                }
+            }
+        }
+
+        private ImageButton connectionButton;
+
+
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
-            // Create your fragment here
         }
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            // Use this to return your custom view for this Fragment
-            return inflater.Inflate(Resource.Layout.Navigation, container, false);
-            //return inflater.Inflate(Resource.Layout.Main, container, false);
+            View view = inflater.Inflate(Resource.Layout.Navigation, container, false);
+
+            view.FindViewById<ImageButton>(Resource.Id.homeButton).Click += homeClicked;
+            view.FindViewById<ImageButton>(Resource.Id.gamesButton).Click += gamesClicked;
+
+            connectionButton = view.FindViewById<ImageButton>(Resource.Id.connectionButton);
+            connectionButton.Click += connectionClicked;
+
+            view.FindViewById<ImageButton>(Resource.Id.settingsButton).Click += settingsClicked;
+
+            return view;
         }
     }
 }

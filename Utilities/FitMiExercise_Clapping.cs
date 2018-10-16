f57d64dd3bf0a39138388e823b6b27258ed2e69b
Patrick-Grady <plgrady07@gmail.com>
2018-10-16 12:39:15 -0500

    Create some more exercise classes

---
+++ Utilities/FitMiExercise_Clapping.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using FitMiAndroid;
+
+namespace Exercises
+{
+    public class FitMiExercise_Clapping : FitMiExerciseBase
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public FitMiExercise_Clapping(HIDPuckDongle p)
+            : base(p)
+        {
+            //empty
+        }
+
+        #endregion
+
+        #region Overrides
+
+        public override void Update()
+        {
+            base.Update();
+
+            //Grab the current force value on the loadcell of the puck
+            var current_force_value = FitMi_Controller.PuckPack0.Loadcell;
+
+
+        }
+
+        #endregion
+    }
+}

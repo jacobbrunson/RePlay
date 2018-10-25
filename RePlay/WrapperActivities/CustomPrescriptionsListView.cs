bf6a8d969d03aab982bee8cced82f64458e18d32
neuroevolutus <sentabel@gmail.com>
2018-10-25 16:50:32 -0500

    Make CustomPrescriptionListView a nested class within SettingsActivity

--- RePlay/WrapperActivities/CustomPrescriptionsListView.cs
+++ RePlay/WrapperActivities/CustomPrescriptionsListView.cs
@@ -13,83 +13,85 @@ using Java.Lang;
 
 namespace RePlay.WrapperActivities
 {
-    [Activity(Label = "CustomPrescriptionsListView")]
-    public class CustomPrescriptionsListView : BaseAdapter
+    public partial class SettingsActivity : Activity
     {
-        private Context Context;
-        private List<Prescription> PrescriptionsList;
-        private bool ContainsLast;
-        private bool isAssigned;
-
-        public CustomPrescriptionsListView(Context mcontext, List<Prescription> prescriptions, bool hasLastElement)
+        public class CustomPrescriptionsListView : BaseAdapter
         {
-            Context = mcontext;
-            PrescriptionsList = prescriptions;
-            ContainsLast = hasLastElement;
-            isAssigned = true;
-        }
+            Context Context;
+            List<Prescription> PrescriptionsList;
+            bool ContainsLast;
+            bool isAssigned;
 
-        public CustomPrescriptionsListView(Context mcontext, List<Prescription> prescriptions)
-        {
-            Context = mcontext;
-            PrescriptionsList = prescriptions;
-            ContainsLast = false;
-            isAssigned = false;
-        }
-
-        public override int Count
-        {
-            get { return PrescriptionsList.Count; }
-        }
-
-        public override Java.Lang.Object GetItem(int position)
-        {
-            return null;
-        }
-
-        public override long GetItemId(int position)
-        {
-            return 0;
-        }
-
-        public override View GetView(int position, View convertView, ViewGroup parent)
-        {
-            View view = convertView;
-
-            if (view == null)
+            public CustomPrescriptionsListView(Context mcontext, List<Prescription> prescriptions, bool hasLastElement)
             {
-                if (ContainsLast == true &&  position == PrescriptionsList.Count - 1)
-                {
-                    view = LayoutInflater.From(Context).Inflate(Resource.Layout.PrescriptionsGridPlus, null, false);
-                    ImageView addPrescription = view.FindViewById<ImageButton>(Resource.Id.add_button);
-                    addPrescription.Click += Add_Prescription_Click;
-                }
-                else if (isAssigned)
-                {
-                    view = LayoutInflater.From(Context).Inflate(Resource.Layout.PrescriptionsGrid, null, false);
-                    Prescription card = PrescriptionsList[position];
-                    TextView ExerciseText = view.FindViewById<TextView>(Resource.Id.exercise_name);
-                    ExerciseText.Text = card.Exercise;
-                    TextView DeviceText = view.FindViewById<TextView>(Resource.Id.device_name);
-                    DeviceText.Text = card.Device;
-                    TextView GameText = view.FindViewById<TextView>(Resource.Id.game_name);
-                    //GameText.Text = card.Game.Name;
-                }
-                else
-                {
-                    view = LayoutInflater.From(Context).Inflate(Resource.Layout.SavedPrescription, null, false);
-                }
+                Context = mcontext;
+                PrescriptionsList = prescriptions;
+                ContainsLast = hasLastElement;
+                isAssigned = true;
             }
 
-            return view;
-        }
+            public CustomPrescriptionsListView(Context mcontext, List<Prescription> prescriptions)
+            {
+                Context = mcontext;
+                PrescriptionsList = prescriptions;
+                ContainsLast = false;
+                isAssigned = false;
+            }
 
-        private void Add_Prescription_Click(object sender, EventArgs e)
-        {
-            Activity settings = (Activity)Context;
-            FragmentTransaction fm = settings.FragmentManager.BeginTransaction();
-            AddPrescriptionFragment dialog = AddPrescriptionFragment.NewInstance();
-            dialog.Show(fm, "dialog fragment");
+            public override int Count
+            {
+                get { return PrescriptionsList.Count; }
+            }
+
+            public override Java.Lang.Object GetItem(int position)
+            {
+                return null;
+            }
+
+            public override long GetItemId(int position)
+            {
+                return 0;
+            }
+
+            public override View GetView(int position, View convertView, ViewGroup parent)
+            {
+                View view = convertView;
+
+                if (view == null)
+                {
+                    if (ContainsLast == true && position == PrescriptionsList.Count - 1)
+                    {
+                        view = LayoutInflater.From(Context).Inflate(Resource.Layout.PrescriptionsGridPlus, null, false);
+                        ImageView addPrescription = view.FindViewById<ImageButton>(Resource.Id.add_button);
+                        addPrescription.Click += Add_Prescription_Click;
+                    }
+                    else if (isAssigned)
+                    {
+                        view = LayoutInflater.From(Context).Inflate(Resource.Layout.PrescriptionsGrid, null, false);
+                        Prescription card = PrescriptionsList[position];
+                        TextView ExerciseText = view.FindViewById<TextView>(Resource.Id.exercise_name);
+                        ExerciseText.Text = card.Exercise;
+                        TextView DeviceText = view.FindViewById<TextView>(Resource.Id.device_name);
+                        DeviceText.Text = card.Device;
+                        TextView GameText = view.FindViewById<TextView>(Resource.Id.game_name);
+                        //GameText.Text = card.Game.Name;
+                    }
+                    else
+                    {
+                        view = LayoutInflater.From(Context).Inflate(Resource.Layout.SavedPrescription, null, false);
+                    }
+                }
+
+                return view;
+            }
+
+            void Add_Prescription_Click(object sender, EventArgs e)
+            {
+                Activity settings = (Activity)Context;
+                FragmentTransaction fm = settings.FragmentManager.BeginTransaction();
+                AddPrescriptionFragment dialog = AddPrescriptionFragment.NewInstance();
+                dialog.Show(fm, "dialog fragment");
+            }
         }
     }
 }

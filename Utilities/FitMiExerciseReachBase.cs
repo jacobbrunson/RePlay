956ef22415621d4c3b985ed4f349e7b87323b18d
Patrick Grady <plgrady07@gmail.com>
2018-10-26 16:37:02 -0500

    Added line to actually switch which puck is input

--- Utilities/FitMiExerciseReachBase.cs
+++ Utilities/FitMiExerciseReachBase.cs
@@ -46,6 +46,7 @@ namespace Exercises
             {
                 current_force_value = FitMi_Controller.PuckPack0.Loadcell;
             }
+            SwitchPuck();
             
             Dictionary<FitMiSensitivity, double> sensitivity_mapping =
                 base.mapSensitivity(new double[] { 1000.0, 900.0, 800.0, 700.0, 600.0, 500.0, 450.0 });

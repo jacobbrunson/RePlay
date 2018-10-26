9e25d00b9cdd9dd8c3d2ffede065d8739fd24143
Patrick Grady <plgrady07@gmail.com>
2018-10-26 16:36:00 -0500

    Create a superclass for reach exercises to handle input from two pucks

---
+++ Utilities/FitMiExerciseReachBase.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using FitMiAndroid;
+
+namespace Exercises
+{
+    public class FitMiExerciseReachBase : FitMiExerciseBase
+    {
+        const bool LEFT = false;
+        const bool RIGHT = true;
+
+        #region private members
+            private bool current_puck { get; set; } = LEFT;
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public FitMiExerciseReachBase(HIDPuckDongle p)
+            : base(p)
+        {
+            //empty
+        }
+
+        #endregion
+
+        #region Overrides
+
+        public override void Update()
+        {
+            base.Update();
+
+            //Grab the current force value on the loadcell of the puck
+            var current_force_value = 0;
+
+            if (current_puck == LEFT)
+            {
+                current_force_value = FitMi_Controller.PuckPack0.Loadcell;
+            }
+            else
+            {
+                current_force_value = FitMi_Controller.PuckPack0.Loadcell;
+            }
+            
+            Dictionary<FitMiSensitivity, double> sensitivity_mapping =
+                base.mapSensitivity(new double[] { 1000.0, 900.0, 800.0, 700.0, 600.0, 500.0, 450.0 });
+            CurrentNormalizedValue = current_force_value / sensitivity_mapping[Sensitivity];
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void SwitchPuck()
+        {
+            current_puck = !current_puck;
+        }
+
+        #endregion
+    }
+}
--- Utilities/FitMiExercise_ReachOut.cs
+++ Utilities/FitMiExercise_ReachOut.cs
@@ -7,7 +7,7 @@ using FitMiAndroid;
 
 namespace Exercises
 {
-    public class FitMiExercise_ReachOut : FitMiExerciseBase
+    public class FitMiExercise_ReachOut : FitMiExerciseReachBase
     {
         #region Constructor
 
@@ -27,15 +27,6 @@ namespace Exercises
         public override void Update()
         {
             base.Update();
-
-            //Grab the current force value on the loadcell of the puck
-            var puck0_force = FitMi_Controller.PuckPack0.Loadcell;
-            var puck1_force = FitMi_Controller.PuckPack1.Loadcell;
-            var current_force_value = Math.Max(puck0_force, puck1_force);
-
-            Dictionary<FitMiSensitivity, double> sensitivity_mapping =
-                base.mapSensitivity(new double[] { 1000.0, 900.0, 800.0, 700.0, 600.0, 500.0, 450.0 });
-            CurrentNormalizedValue = current_force_value / sensitivity_mapping[Sensitivity];
         }
 
         #endregion

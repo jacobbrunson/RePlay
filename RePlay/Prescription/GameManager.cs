a31ab0822fa201878ef05208e1a50bf7b3b4905d
neuroevolutus <sentabel@gmail.com>
2018-11-08 15:01:29 -0600

    Move RePlayGame class into its own file

--- RePlay/Prescription/GameManager.cs
+++ RePlay/Prescription/GameManager.cs
@@ -64,16 +64,4 @@ namespace RePlay
             }
         }
     }
-
-    public class RePlayGame
-    {
-        public readonly string Name;
-        public readonly string AssetNamespace;
-
-        public RePlayGame(string name, string assetNamespace)
-        {
-            Name = name;
-            AssetNamespace = assetNamespace;
-        }
-    }
 }
---
+++ RePlay/Prescription/RePlayGame.cs
@@ -0,0 +1,13 @@
+namespace RePlay {
+    public class RePlayGame
+    {
+        public readonly string Name;
+        public readonly string AssetNamespace;
+
+        public RePlayGame(string name, string assetNamespace)
+        {
+            Name = name;
+            AssetNamespace = assetNamespace;
+        }
+    }
+}

3718ecaa0a847a4c820afd4c7012eafffb4f7f24
Patrick Grady <plgrady07@gmail.com>
2018-11-13 16:01:38 -0600

    Keeping track of state for running prescriptions

--- RePlay/WrapperActivities/PromptActivity.cs
+++ RePlay/WrapperActivities/PromptActivity.cs
@@ -16,23 +16,65 @@ namespace RePlay.WrapperActivities
     [Activity(Label = "PromptActivity", ScreenOrientation = Android.Content.PM.ScreenOrientation.Landscape)]
     public class PromptActivity : Activity
     {
-        
+        // logic
+        int index;
+        List<Prescription> prescription;
+
+        // ui
+        ImageButton next;
+
+        const int REQUEST_CODE = 5432;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             SetContentView(Resource.Layout.Prompt);
 
 
             base.OnCreate(savedInstanceState);
-            
-            this.FindViewById<ImageButton>(Resource.Id.next).Click += delegate {
-                Intent intent = new Intent(this, typeof(WrapperActivities.GamesListActivity));
-                StartActivity(intent);
-            };
+
+            next = this.FindViewById<ImageButton>(Resource.Id.next);
 
             this.FindViewById<ImageButton>(Resource.Id.cancel).Click += delegate {
                 Intent intent = new Intent(this, typeof(MainActivity));
                 StartActivity(intent);
             };
+
+            prescription = PrescriptionManager.Instance;
+            index = StateManager.Instance.Index;            
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            if(requestCode == REQUEST_CODE && resultCode == Result.Ok)
+            {
+                index++;
+
+                if (index < prescription.Count)
+                {
+                    UpdateView();
+                    StateManager.Instance.UpdateState(DateTimeOffset.Now.ToUnixTimeMilliseconds(), index);
+                    // load prescription[i].view
+                    next.Click += delegate
+                    {
+                        // Intent intent = new Intent(this, typeof(prescription[i].Game));
+                        Intent intent = new Intent(this, typeof(WrapperActivities.GamesListActivity));
+                        intent.PutExtra("exercise", prescription[index].Exercise);
+                        intent.PutExtra("duration", prescription[index].Duration);
+                        StartActivityForResult(intent, REQUEST_CODE);
+                    };
+                }
+                else
+                {
+                    //Intent intent = new Intent(this, typeof(PrescriptionDoneActivity));
+                    //StartActivity(intent);
+                }
+            }
+        }
+
+        // update the views pictures based on prescription[index]
+        public void UpdateView()
+        {
+
         }
     }
 }
